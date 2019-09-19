9a336de00d069281f1f65cac69fd88e720cb72af
bkh <bkh751@gmail.com>
2019-09-20 01:40:57 +0900

    손님 to robot, 식당master to robot 인터페이스 구현...

--- interface/robot-server/server.py
+++ interface/robot-server/server.py
@@ -6,15 +6,44 @@ cors = CORS(app, resources={
   r"/*": {"origin": "*"},
 })
 
-db = [{"content":"asdf"},{"content":"asdf2"},{"content":"asdf3"}]
+db = [{"content":"김치찌개주문되었습니다, 곧 로봇이 음식을 가져다 드리겠습니다"},{"content":"오무라이스주문되었습니다"}]
 
+ordered = False
+serving = False
 
-@app.route("/api/slideContent/<slideNo>", methods=["GET"])
-def getSlideContent(slideNo):
-    res = db[int(slideNo)-1]
+@app.route("/api/order/food/<foodNum>", methods=["GET"])
+def getSlideContent(foodNum):
+  global ordered
+  if not ordered:
+    res = db[int(foodNum)-1]
+    ordered = True
     return res['content']
+  elif ordered:
+    return "로봇이 바쁩니다"
+
+@app.route("/api/reset/order/", methods=["GET"])
+def resetOrder():
+  global ordered
+  ordered = False
+  return "reset 되었습니다"
+
+@app.route("/api/serving/start/<tableNo>",methods=["GET"])
+def servingStart(tableNo):
+  global serving
+  if not serving:
+    serving = True
+    return tableNo + " 번 테이블로의 서빙이 시작되었습니다"
+  elif serving:
+    return "지금 로봇은 서빙중에 있습니다."
+
+@app.route("/api/reset/serving/", methods=["GET"])
+def resetServing():
+  global serving
+  serving = False
+  return "reset 되었습니다"
+
 
 
 if __name__ == '__main__':
-    app.run(host='namlonx',port=5000)
-    # app.run(host='localhost',port=5000)
+  app.run(host='0.0.0.0',port=5000)
+  # app.run(host='localhost',port=5000)

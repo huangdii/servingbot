37e641752be9ca47a3f7c44ba84ae359981f6956
bkh <bkh751@gmail.com>
2019-09-19 10:20:46 +0900

    ubuntu 서버간 통신확인이 필요함

---
+++ interface/robot-server/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, resources={
+  r"/*": {"origin": "*"},
+})
+
+db = [{"content:asdf"},{"content:asdf2"},{"content:asdf3"}]
+
+
+@app.route("/api/slideContent/<slideNo>", methods=["GET"])
+def getSlideContent(slideNo):
+    res = db[int(slideNo)-1]
+    return res['content']
+
+
+if __name__ == '__main__':
+    app.run(host='bkhLaptop',port=5000)
+    # app.run(host='localhost',port=5000)
